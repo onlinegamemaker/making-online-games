e848fbb26d73fa598cf0579fa32f42cdd872e4ac
Michael Rätzel <Viir@Viir.de>
2019-05-10 07:49:10 +0000

    Add documentation and refine names

--- implement/elm/package/simplegamedev/src/Simplegamedev20190510.elm
+++ implement/elm/package/simplegamedev/src/Simplegamedev20190510.elm
@@ -1,12 +1,23 @@
 module Simplegamedev20190510 exposing
-    ( KeyboardEvent
-    , SimpleGame
-    , SimpleGameConfiguration
-    , listDictGet
-    , listRemoveSet
-    , simpleGame
-    , svgRectFrom_Fill_Left_Top_Width_Height
+    ( composeSimpleGame, SimpleGame, SimpleGameComposition, KeyboardEvent
+    , listRemoveSet, listDictGet, svgRectFrom_Fill_Left_Top_Width_Height
     )
+
+{-| This library helps you get implement your video game as simple as possible.
+
+
+# Composing the App
+
+@docs composeSimpleGame, SimpleGame, SimpleGameComposition, KeyboardEvent
+
+
+# Common Helpers
+
+Following are generic helper functions which are not specific to one particular game.
+
+@docs listRemoveSet, listDictGet, svgRectFrom_Fill_Left_Top_Width_Height
+
+-}
 
 import Browser
 import Browser.Events
@@ -25,7 +36,37 @@ import Time
 -}
 
 
-type alias SimpleGameConfiguration appState eventFromHtml =
+{-| Use this function to describe how your game is composed of the specific functions in your project.
+Following is an example:
+
+    game : SimpleGame GameState ()
+    game =
+        composeSimpleGame
+            { updateIntervalInMilliseconds = 125
+            , updatePerInterval = updatePerInterval
+            , updateOnKeyDown = updateOnKeyDown
+            , updateOnKeyUp = updateOnKeyUp
+            , renderToHtml = renderToHtml
+            , initialState = initialGameState
+            , updateForEventFromHtml = updateForEventFromHtml
+            }
+
+-}
+composeSimpleGame :
+    SimpleGameComposition appState eventFromHtml
+    -> Program () appState (SimpleGameWithKeyboardInputAndFixedUpdateIntervalEvent eventFromHtml)
+composeSimpleGame appConfig =
+    Browser.element
+        { init = always ( appConfig.initialState, Cmd.none )
+        , view = appConfig.renderToHtml >> Html.map EventFromHtml
+        , update = \event model -> ( simpleGameWithKeyboardInputAndFixedUpdateIntervalUpdate appConfig event model, Cmd.none )
+        , subscriptions = simpleGameWithKeyboardInputAndFixedUpdateIntervalSubscriptions appConfig
+        }
+
+
+{-| Describes how your game app is composed of functions that describe how to handle the different kinds of events like for example, pressing or releasing key.
+-}
+type alias SimpleGameComposition appState eventFromHtml =
     { updateIntervalInMilliseconds : Int
     , updatePerInterval : appState -> appState
     , updateOnKeyDown : Keyboard.Event.KeyboardEvent -> appState -> appState
@@ -36,35 +77,19 @@ type alias SimpleGameConfiguration appState eventFromHtml =
     }
 
 
-type SimpleGameWithKeyboardInputAndFixedUpdateIntervalEvent eventFromHtml
-    = ArriveAtTime Time.Posix
-    | KeyDown Keyboard.Event.KeyboardEvent
-    | KeyUp Keyboard.Event.KeyboardEvent
-    | EventFromHtml eventFromHtml
+{-| This type helps you write a type annotation for the function describing the composition of your game:
 
+    game : SimpleGame GameState ()
+    game =
+    ....
 
+-}
 type alias SimpleGame appState eventFromHtml =
     Program () appState (SimpleGameWithKeyboardInputAndFixedUpdateIntervalEvent eventFromHtml)
 
 
-type alias KeyboardEvent =
-    Keyboard.Event.KeyboardEvent
-
-
-simpleGame :
-    SimpleGameConfiguration appState eventFromHtml
-    -> Program () appState (SimpleGameWithKeyboardInputAndFixedUpdateIntervalEvent eventFromHtml)
-simpleGame appConfig =
-    Browser.element
-        { init = always ( appConfig.initialState, Cmd.none )
-        , view = appConfig.renderToHtml >> Html.map EventFromHtml
-        , update = \event model -> ( simpleGameWithKeyboardInputAndFixedUpdateIntervalUpdate appConfig event model, Cmd.none )
-        , subscriptions = simpleGameWithKeyboardInputAndFixedUpdateIntervalSubscriptions appConfig
-        }
-
-
 simpleGameWithKeyboardInputAndFixedUpdateIntervalUpdate :
-    SimpleGameConfiguration appState eventFromHtml
+    SimpleGameComposition appState eventFromHtml
     -> SimpleGameWithKeyboardInputAndFixedUpdateIntervalEvent eventFromHtml
     -> appState
     -> appState
@@ -84,7 +109,7 @@ simpleGameWithKeyboardInputAndFixedUpdateIntervalUpdate appConfig event appState
 
 
 simpleGameWithKeyboardInputAndFixedUpdateIntervalSubscriptions :
-    SimpleGameConfiguration appState eventFromHtml
+    SimpleGameComposition appState eventFromHtml
     -> appState
     -> Sub (SimpleGameWithKeyboardInputAndFixedUpdateIntervalEvent eventFromHtml)
 simpleGameWithKeyboardInputAndFixedUpdateIntervalSubscriptions appConfig appState =
@@ -94,15 +119,34 @@ simpleGameWithKeyboardInputAndFixedUpdateIntervalSubscriptions appConfig appStat
         |> Sub.batch
 
 
+type SimpleGameWithKeyboardInputAndFixedUpdateIntervalEvent eventFromHtml
+    = ArriveAtTime Time.Posix
+    | KeyDown Keyboard.Event.KeyboardEvent
+    | KeyUp Keyboard.Event.KeyboardEvent
+    | EventFromHtml eventFromHtml
 
--- Following are generic helper functions which are not specific to one particular game. Also based on the snake game example as documented at https://ellie-app.com/5nvrmZh8WHca1
+
+{-| This type describes the keyboard events as used in the functions `updateOnKeyDown` and `updateOnKeyUp`.
+Use as follows:
+
+    updateOnKeyDown : KeyboardEvent -> GameState -> GameState
+    updateOnKeyDown =
+    ...
+
+-}
+type alias KeyboardEvent =
+    Keyboard.Event.KeyboardEvent
 
 
+{-| Remove a set of values from a list
+-}
 listRemoveSet : List a -> List a -> List a
 listRemoveSet elementsToRemove =
     List.filter (\element -> elementsToRemove |> List.member element |> not)
 
 
+{-| Get the value matching the given key out of a dictionary.
+-}
 listDictGet : key -> List ( key, value ) -> Maybe value
 listDictGet key =
     List.filterMap
@@ -116,6 +160,12 @@ listDictGet key =
         >> List.head
 
 
+{-| Generate the HTML code for a SVG rectangle. This only works when placed in an SVG element.
+The follow example shows how to create a red rectangle with the upper left corner at coordinates 10|10, a width of 7 and a height of 4 :
+
+    svgRectFrom_Fill_Left_Top_Width_Height "red" ( 10, 10 ) ( 7, 4 )
+
+-}
 svgRectFrom_Fill_Left_Top_Width_Height : String -> ( Int, Int ) -> ( Int, Int ) -> Svg.Svg a
 svgRectFrom_Fill_Left_Top_Width_Height fill ( left, top ) ( width, height ) =
     Svg.rect
