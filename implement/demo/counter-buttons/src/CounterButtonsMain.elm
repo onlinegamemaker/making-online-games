ce102470b4544c4c0dda6daef2fc2660b3e20b06
Michael Rätzel <viir@viir.de>
2023-09-04 19:14:23 +0000

    Add simple Elm app with buttons

---
+++ implement/demo/counter-buttons/src/CounterButtonsMain.elm
@@ -0,0 +1,61 @@
+module CounterButtonsMain exposing (..)
+
+{-
+   This ia a simple app showing two buttons to increment and decrement an integer in the app state.
+
+   For more examples, including complete games, see <https://github.com/onlinegamemaker/making-online-games>
+-}
+
+import Browser
+import Html
+import Html.Attributes
+import Html.Events
+
+
+type alias State =
+    Int
+
+
+type Event
+    = Increment
+    | Decrement
+
+
+main : Program () State Event
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+init : State
+init =
+    0
+
+
+update : Event -> State -> State
+update event state =
+    case event of
+        Increment ->
+            state + 1
+
+        Decrement ->
+            state - 1
+
+
+view : State -> Html.Html Event
+view state =
+    Html.div
+        [ Html.Attributes.style "background" "#111"
+        , Html.Attributes.style "color" "whitesmoke"
+        ]
+        [ Html.button
+            [ Html.Events.onClick Decrement ]
+            [ Html.text "-" ]
+        , Html.div
+            [ Html.Attributes.style "display" "inline-block"
+            , Html.Attributes.style "padding" "0 1em"
+            ]
+            [ Html.text (String.fromInt state) ]
+        , Html.button
+            [ Html.Events.onClick Increment ]
+            [ Html.text "+" ]
+        ]
